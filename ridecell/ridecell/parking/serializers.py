3e8532bc1f0402d5297df1ce22ecf8f5dfbb5354
gary <garyguo110@gmail.com>
2016-10-23 15:14:44 -0400

    return all fields for parking location

--- ridecell/ridecell/parking/serializers.py
+++ ridecell/ridecell/parking/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class ParkingLocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = ParkingLocation
-        fields = ('description', 'id', 'location', 'price')
+        fields = ('description', 'id', 'location', 'price', 'reserved', 'capacity')
